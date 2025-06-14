bd983a03dfeb8bc89b6da17451f827ee74f746ce
sumit <roy184433@gmail.com>
2025-06-14 18:37:05 +0530

    adding docker files

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello, React in Docker!</h1>
+            <ol>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+            </ol>
+        </div>
+    );
+}
+
+export default App;
