7578b3112b51f40ffac4f7e4637b0f934427f219
shruti <shruti040112@gmail.com>
2025-06-19 20:10:39 +0530

    hig

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -75,7 +75,7 @@ function App() {
             <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
               <div className="flex items-center justify-between">
                 <a href="#" className="-m-1.5 p-1.5">
-                  <span className="sr-only">Your Company</span>
+                  <span className="sr-only">Our project</span>
                   <img
                     alt=""
                     src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
